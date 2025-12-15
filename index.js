c3af698a93894e3ab367128d3d8973bf11b183e9
Jemp9791 <jennyocampos@gmail.com>
2025-12-15 09:11:03 -0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import TelegramBot from "node-telegram-bot-api";
 
-const BOT_TOKEN = process.env.8130447159:AAHxzp5S1lcgYOemw5dgF5V1DGh141dHmkA;
+const BOT_TOKEN = process.env.Bot_Token;
 const GAS_URL = process.env.https://script.google.com/macros/s/AKfycbwfYMqsIWzGDB8UiQx30XoV8K4yes54tJo2Rcb512Ku2ce9vgOL7OgOflE6fc90rnb0/exec;
 const WEBHOOK_URL = process.env.https://ezerbot-system.onrender.com; // OBLIGATORIO: https://tuapp.onrender.com (sin /webhook)
 
