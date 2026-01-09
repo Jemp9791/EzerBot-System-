3da3ef8c5ccdd1106cefbd6fa031b9ecd791a5d8
Jemp9791 <jennyocampos@gmail.com>
2026-01-09 09:57:38 -0300

    categoriesHandler.js

--- src/handlers/categoriesHandler.js
+++ src/handlers/categoriesHandler.js
@@ -1,1 +1,20 @@
 
+const CatalogService = require('../modules/catalog/catalogService');
+const catalog = new CatalogService();
+
+async function categoriesHandler(ctx) {
+  const productos = await catalog.getAllProducts();
+
+  const categorias = [...new Set(productos.map(p => p.CATEGORIA))];
+
+  await ctx.reply('Elegí una categoría:', {
+    reply_markup: {
+      inline_keyboard: categorias.map(c => [
+        { text: c, callback_data: `categoria_${c}` }
+      ])
+    }
+  });
+}
+
+module.exports = categoriesHandler;
+
