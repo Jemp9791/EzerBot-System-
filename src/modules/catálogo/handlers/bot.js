3dcfd375e96f0e667f9076e3a46cd7b0a90839b4
Jemp9791 <jennyocampos@gmail.com>
2026-01-09 09:23:36 -0300

    bot.js

--- src/modules/catálogo/handlers/bot.js
+++ src/modules/catálogo/handlers/bot.js
@@ -1,15 +1,1 @@
-const navigationHandler = require('./handlers/navigationHandler');
 
-bot.on('callback_query', async ctx => {
-  const data = ctx.callbackQuery.data;
-
-  if (data.startsWith('categoria_')) {
-    return categoryCarouselHandler(ctx);
-  }
-
-  if (data.startsWith('next_') || data.startsWith('prev_')) {
-    return navigationHandler(ctx);
-  }
-
-  // después agregamos el botón "Quiero este"
-});
