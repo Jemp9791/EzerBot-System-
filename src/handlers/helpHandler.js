9020309ecdef74922ae8d53eebb87182c03ea6ad
Jemp9791 <jennyocampos@gmail.com>
2026-01-09 10:04:54 -0300

    helpHandler.js

--- src/handlers/helpHandler.js
+++ src/handlers/helpHandler.js
@@ -1,1 +1,22 @@
 
+
+const ConfigService = require('../modules/config/configService');
+const config = new ConfigService();
+
+async function helpHandler(ctx) {
+  const gif = await config.getValue('SellosURL'); 
+  const texto = await config.getValue('TextoBienvenida');
+
+  await ctx.replyWithAnimation(gif, {
+    caption: texto,
+    parse_mode: 'Markdown',
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: '📞 WhatsApp', url: await config.getValue('WhatsAppLink') }],
+        [{ text: '📩 Email', url: `mailto:${await config.getValue('EmailSistema')}` }]
+      ]
+    }
+  });
+}
+
+module.exports = helpHandler;
