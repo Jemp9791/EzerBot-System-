f7b26f9a49b40396e5925d1a097075e57e765af5
Jemp9791 <jennyocampos@gmail.com>
2026-01-09 07:47:01 -0300

    Delete src directory

--- src/sheets.js
+++
@@ -1,8 +0,0 @@
-  } else {
-    // Si existe pero está vacía, escribir headers
-    const firstRow = await getSheetValues(`${sheetName}!A1:Z1`);
-    if (!firstRow.length || firstRow[0].join("").trim() === "") {
-      await setSheetValues(`${sheetName}!A1`, [headers]);
-    }
-  }
-}
