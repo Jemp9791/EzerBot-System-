33a4f20bb6f4ee7ba7d97c97c4d7260764a01ee7
Jemp9791 <jennyocampos@gmail.com>
2026-01-09 10:25:47 -0300

    configService.js

--- src/modules/config/configService.js
+++ src/modules/config/configService.js
@@ -1,1 +1,22 @@
 
+const axios = require('axios');
+
+class ConfigService {
+  constructor() {
+    this.url = process.env.CONFIG_URL;
+  }
+
+  async getAll() {
+    const res = await axios.get(this.url);
+    return res.data;
+  }
+
+  async getValue(key) {
+    const config = await this.getAll();
+    const item = config.find(row => row.CLAVE === key);
+    return item ? item.VALOR : null;
+  }
+}
+
+module.exports = ConfigService;
+
