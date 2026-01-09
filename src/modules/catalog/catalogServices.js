ff0f0fcf0e2f64eef7eaebf302156dea4e5e9205
Jemp9791 <jennyocampos@gmail.com>
2026-01-09 10:19:02 -0300

    catalogServices.js

--- src/modules/catalog/catalogServices.js
+++ src/modules/catalog/catalogServices.js
@@ -1,1 +1,21 @@
 
+const axios = require('axios');
+
+class CatalogService {
+  constructor() {
+    this.url = process.env.CATALOGO_URL;
+  }
+
+  async getAllProducts() {
+    const res = await axios.get(this.url);
+    return res.data;
+  }
+
+  async getProductsByCategory(categoria) {
+    const productos = await this.getAllProducts();
+    return productos.filter(p => p.CATEGORIA === categoria);
+  }
+}
+
+module.exports = CatalogService;
+
