37d30649b2ae75485f84383a3aa05926f6f695bf
Jemp9791 <jennyocampos@gmail.com>
2026-01-10 13:35:29 -0300

    startHandler.js

--- src/handlers/startHandler.js
+++ src/handlers/startHandler.js
@@ -1,25 +1,27 @@
-
-const ConfigService = require('../modules/config/configService');
-const config = new ConfigService();
-
 async function startHandler(ctx) {
-  const t1 = await config.getValue('TextoInicio');
-  const t2 = await config.getValue('TextoInicio2');
-  const t3 = await config.getValue('TextoInicio3');
-  const gif = await config.getValue('CardURL');
+  const texto = '👋 Bienvenida al bot';
+  const gifId = null; // o lo que venga de config
 
-  await ctx.replyWithAnimation(gif, {
-    caption: `${t1}\n\n${t2}\n\n${t3}`,
-    parse_mode: 'Markdown',
-    reply_markup: {
-      keyboard: [
-        ['🧀 Catálogo', '🎟️ Sellos'],
-        ['ℹ️ Ayuda', '📣 Compartir']
-      ],
-      resize_keyboard: true
-    }
-  });
+  if (gifId) {
+    await ctx.replyWithAnimation(gifId, {
+      caption: texto,
+      reply_markup: {
+        keyboard: [
+          ['📦 Catálogo', '🎟️ Sellos'],
+          ['ℹ️ Ayuda', '📣 Compartir']
+        ],
+        resize_keyboard: true
+      }
+    });
+  } else {
+    await ctx.reply(texto, {
+      reply_markup: {
+        keyboard: [
+          ['📦 Catálogo', '🎟️ Sellos'],
+          ['ℹ️ Ayuda', '📣 Compartir']
+        ],
+        resize_keyboard: true
+      }
+    });
+  }
 }
-
-module.exports = startHandler;
-      
