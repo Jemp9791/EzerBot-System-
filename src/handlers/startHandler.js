2f5198268fc4936e6a58f2c0150b17890d730cc2
Jemp9791 <jennyocampos@gmail.com>
2026-01-10 16:16:38 -0300

    startHandler.js

--- src/handlers/startHandler.js
+++ src/handlers/startHandler.js
@@ -1,19 +1,8 @@
 module.exports = async (ctx) => {
-  console.log('📥 /start recibido');
-
   await ctx.reply(
-    '👋 Hola Jenny!\n\n' +
-    'Bienvenida a *EzerBot* 🤖\n\n' +
-    'Usá el menú para continuar.',
-    {
-      parse_mode: 'Markdown',
-      reply_markup: {
-        keyboard: [
-          ['📦 Catálogo', '🎟 Sellos'],
-          ['🆘 Ayuda', '📤 Compartir']
-        ],
-        resize_keyboard: true
-      }
-    }
+    '📤 *Compartir Todo Queso*\n\n' +
+    'Recomendá el negocio compartiendo este link:\n' +
+    'https://t.me/Todo_Queso_bot',
+    { parse_mode: 'Markdown' }
   );
 };
