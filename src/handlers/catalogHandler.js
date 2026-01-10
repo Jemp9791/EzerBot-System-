8cf9fddd2c94ac0b1782d149646b11b27014538d
Jemp9791 <jennyocampos@gmail.com>
2026-01-10 20:53:24 -0300

    catalogHandler.js

--- src/handlers/catalogHandler.js
+++ src/handlers/catalogHandler.js
@@ -1,35 +1,40 @@
 // src/handlers/catalogHandler.js
 
+const catalogService = require("../modules/catalog/catalogService");
 const config = require("../modules/config/configService");
 
 async function show(phone) {
-  const categorias = await config.list("CategoriasCatalogo");
+  const categorias = await catalogService.obtenerCategorias();
   const gif = await config.get("GifCatalogo");
 
   let texto = "🧀 *Nuestro catálogo*\n\n";
+
   categorias.forEach(cat => {
     texto += `• ${cat}\n`;
   });
 
-  texto += `\n👉 Decime qué categoría te interesa`;
+  texto += "\n👉 Decime qué categoría te interesa y te muestro opciones";
 
-  return [
+  const mensajes = [
     {
       to: phone,
       type: "text",
       text: { body: texto },
     },
-    gif
-      ? {
-          to: phone,
-          type: "image",
-          image: {
-            link: gif,
-            caption: "👆 Elegí tranquilo, te ayudo a armar el pedido 😉",
-          },
-        }
-      : null,
-  ].filter(Boolean);
+  ];
+
+  if (gif) {
+    mensajes.push({
+      to: phone,
+      type: "image",
+      image: {
+        link: gif,
+        caption: "👆 Elegí tranquilo, te ayudo a armar el pedido 😉",
+      },
+    });
+  }
+
+  return mensajes;
 }
 
 module.exports = {
