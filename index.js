3369c02f4874435184e1712e5237f70a0f1988fc
Jemp9791 <jennyocampos@gmail.com>
2026-01-09 22:45:43 -0300

    index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
+// Handlers (nombres EXACTOS según tu repo)
 const catalogHandler = require('./src/handlers/catalogHandler.js');
 const carritoHandler = require('./src/handlers/carritoHandler.js');
 const checkoutHandler = require('./src/handlers/checkoutHandler.js');
-const adminConfirmHandler = require('./src/handlers/adminConfirmHandler.js');
+const adminConfirmHandler = require('./src/handlers/admindConfirmHandler.js'); // así está en tu repo
 
 const token = process.env.TelegramBotToken;
+
+// Bot en modo polling (necesario para Web Service)
 const bot = new TelegramBot(token, { polling: true });
 
-// Comandos
+// Comando /start
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, '¡Bienvenido a "TODO QUESO 🧀"!', {
+  bot.sendMessage(msg.chat.id, '¡Bienvenido a EZERBOT!', {
     reply_markup: {
       keyboard: [['Menú']],
       resize_keyboard: true
@@ -19,9 +22,22 @@ bot.onText(/\/start/, (msg) => {
   });
 });
 
-bot.onText(/\/catalogo/, (msg) => catalogHandler(bot, msg));
-bot.onText(/\/carrito/, (msg) => carritoHandler(bot, msg));
-bot.onText(/\/checkout/, (msg) => checkoutHandler(bot, msg));
+// Comando /catalogo
+bot.onText(/\/catalogo/, (msg) => {
+  catalogHandler(bot, msg);
+});
 
-// Mensajes del vendedor (Confirmar/Rechazar)
-bot.on('message', (msg) => adminConfirmHandler(bot, msg));
+// Comando /carrito
+bot.onText(/\/carrito/, (msg) => {
+  carritoHandler(bot, msg);
+});
+
+// Comando /checkout
+bot.onText(/\/checkout/, (msg) => {
+  checkoutHandler(bot, msg);
+});
+
+// Mensajes del vendedor (confirmar/rechazar)
+bot.on('message', (msg) => {
+  adminConfirmHandler(bot, msg);
+});
